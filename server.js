b412d66ed0a6d446c833287b22b5208af2e0959e
achill06 <azilsyed06@gmail.com>
2026-09-18 01:26:24 +0530

    Configure production CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: ["http://localhost:5173",process.env.FRONTEND_URL,].filter(Boolean),
   credentials: true,
 }));
 
