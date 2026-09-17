9c0e743668e9e7cfe68ea963b97fe6487e76b1ee
achill06 <azilsyed06@gmail.com>
2026-09-18 00:36:55 +0530

    Enable SSL for Render PostgreSQL

--- tools/db-connection.js
+++ tools/db-connection.js
@@ -1,6 +1,11 @@
 const { Pool } = require('pg');
 const config = require('./config.js');
 
-const pool = new Pool({ connectionString: config.postgres.connectionString });
+const pool = new Pool({
+  connectionString: config.postgres.connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = { pool };
