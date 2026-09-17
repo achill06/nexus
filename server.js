0ee3dd8029bd3c8d8b0c796953b74e77781f503d
achill06 <azilsyed06@gmail.com>
2026-09-18 01:15:58 +0530

    Allow Vercel frontend

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use('/auth', authRoutes);
 app.use('/matching',matchingRoutes);
